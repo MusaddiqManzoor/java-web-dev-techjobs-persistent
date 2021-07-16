c302957e7f9eaccb3980b61eadb1166b71a0ad10
Musaddiqmanzoor <musaddiqmanzoor4@gmail.com>
2021-07-15 19:37:38 -0500

    first-swl-query-fixed

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 ## Part 1: Test it with SQL
-SELECT * FROM job.columns;
-SELECT * FROM job.types;
+--Columns: id, name, employer_id
+--Types: int, varchar(50), int
 
 ## Part 2: Test it with SQL
 SELECT name FROM employer WHERE location LIKE 'S%Louis%';
